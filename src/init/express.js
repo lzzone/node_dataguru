a6276dff1158722b337cf8716481b77755d8b95f
郭宏宽 <guohongkuan@ainirobot.com>
2017-06-29 02:22:14 +0800

    输出调试信息

--- src/init/express.js
+++ src/init/express.js
@@ -12,6 +12,10 @@ import bodyParser from 'body-parser';
 import multiparty from 'multiparty';
 
 module.exports = function(done) {
+
+    const debug = $.createDebug('init:express');
+    debug("initing Express...");
+
     const app = express();
 
     app.use(bodyParser.json());
